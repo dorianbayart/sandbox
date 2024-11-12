f3047d7f6b5165144e9a8db7c52a6a57f6bbac56
Dorian Bayart <dorian.bayart@gmail.com>
2024-11-12 17:59:40 +0100

    Reveal items progressively

--- link-in-bio/script.js
+++ link-in-bio/script.js
@@ -3,26 +3,51 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(() => document.querySelectorAll('body')[0].style.opacity = '1', 250)
-  //document.querySelectorAll('body')[0].style.opacity = '1'
 
-  const profilePicture = document.getElementById('profilePicture')
-  profilePicture.addEventListener('canplay', () => {
-    profilePicture.style.opacity = '1'
-  }, true)
-  profilePicture.addEventListener('loadedmetadata', () => {
-    profilePicture.style.opacity = '1'
-  }, true)
+  // Reveal hidden items progressively
+  document.querySelectorAll('*[style="opacity: 0"]').forEach((item, i) => {
+    setTimeout(() => {
+      console.log(item)
+      if(item.tagName !== 'VIDEO') revealObject(item)
+    }, (i+1) * 200)
+  })
+  
 
-  const source = document.querySelectorAll('#profilePicture source')[0]
-  source.addEventListener('error', () => {
-    const img = document.querySelectorAll('#profilePicture img')[0]
-    img.addEventListener('load', () => {
-      img.parentElement.style.opacity = '1'
-    }, true)
-  }, true)
-
-
+  // Update the footer with the current year date
   const currentYear = new Date().getFullYear()
   document.getElementById('footer').innerHTML = `&copy; ${currentYear} - Made with &#9829; by <a href="https://github.com/dorianbayart" title="DorianBayart on GitHub">0xDBA</a>`
+})
+
+// Reveal function
+const revealObject = async (htmlObject) => {
+  htmlObject.style.opacity = '1'
+}
+
+const profilePicture = document.getElementById('profilePicture')
+
+// Periodically check the Video state object to reveal it when it's ready
+const timer = setInterval(() => {
+  if(profilePicture.currentTime > 0 || profilePicture.readyState > 0) {
+    revealObject(profilePicture)
+    clearInterval(timer)
+  }
+}, 400)
+
+const source = document.querySelectorAll('#profilePicture source')[0]
+source.addEventListener('error', () => {
+  console.debug('Error Event')
+  const img = document.querySelectorAll('#profilePicture img')[0]
+  img.addEventListener('load', () => {
+    console.debug('Img Loaded Event')
+    revealObject(img.parentElement)
+  })
+})
+
+source.addEventListener('stalled', () => {
+  console.debug('Stalled Event')
+  const img = document.querySelectorAll('#profilePicture img')[0]
+  img.addEventListener('load', () => {
+    console.debug('Img Loaded Event')
+    revealObject(img.parentElement)
+  })
 })
