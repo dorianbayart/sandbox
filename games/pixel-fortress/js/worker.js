dcd12ed6aea76b8710d860bcbbe88d18cb30904c
dorian.bayart <dorian.bayart@gmail.com>
2024-12-03 10:51:24 +0000

    Update worker.js

--- games/pixel-fortress/js/worker.js
+++ games/pixel-fortress/js/worker.js
@@ -3,16 +3,15 @@
 let canvas = null
 let ctx = null
 
-// Waiting to receive the OffScreenCanvas
 self.onmessage = (event) => {
   if (event.data === "draw") {
     draw()
   } else {
     canvas = event.data.canvas
-    ctx = canvas.getContext("2d")
+    ctx = canvas?.getContext("2d")
   }
 }
 
 const draw = () => {
-  
+
 }
