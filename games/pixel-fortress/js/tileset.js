a9638a14aab131fc9ccf31ceffca5859c8e5c171
Dorian Bayart <dorian.bayart@gmail.com>
2024-12-18 18:12:42 +0100

    Improve map generation

--- games/pixel-fortress/js/tileset.js
+++ games/pixel-fortress/js/tileset.js
@@ -96,51 +96,135 @@ let timings = new Array(50).fill(10), drawMainTimings = new Array(50).fill(10)
 // }
 
 const generateMap = async () => {
-  const noise = new PerlinNoise(performance.now())
+  const noise = new PerlinNoise(Math.random())
   
-  // Terrain type parameters
   const TERRAIN_TYPES = {
-      WATER: { weight: MAX_WEIGHT, spriteRange: { x: [0, 1], y: [26, 27] } },
-      ROCK: { weight: MAX_WEIGHT, spriteRange: { x: [0, 1], y: [26, 26] } },
-      TREE: { weight: MAX_WEIGHT, spriteRange: { x: [2, 3], y: [26, 27] } },
-      GRASS: { weight: 1, spriteRange: { x: [0, 2], y: [0, 2] } },
-      //END: { weight: -2, spriteRange: { x: [spriteCoords_End.x], y: [spriteCoords_End.y] } }
-  };
+    WATER: { type: 'WATER', weight: MAX_WEIGHT, spriteRange: { x: [0, 0], y: [17, 17] } },
+    ROCK: { type: 'ROCK', weight: MAX_WEIGHT, spriteRange: { x: [0, 1], y: [26, 26] } },
+    TREE: { type: 'TREE', weight: MAX_WEIGHT, spriteRange: { x: [2, 3], y: [26, 27] } },
+    GRASS: { type: 'GRASS', weight: 1, spriteRange: { x: [0, 2], y: [0, 2] } },
+    SAND: { type: 'SAND', weight: 1, spriteRange: { x: [3, 3], y: [3, 3] } },
+  }
 
-  const NOISE_SCALE = 0.25;  // Controls terrain smoothness
+  const NOISE_SCALE = 0.175; // Controls terrain smoothness
   const TERRAIN_THRESHOLD = {
-      WATER: 0.2,
-      ROCK: 0.45,
-      TREE: 0.44,
-      GRASS: 0.9
-  };
+    WATER: 0.33,
+    ROCK: 0.34,
+    TREE_NEXT_TO_WATER: 0.35,
+    GRASS_NEXT_TO_WATER: 0.45,
+    SAND: 0.47,
+    GRASS: 0.56,
+    TREE: 1,
+  }
 
   for (let x = 0; x < MAP_WIDTH; x++) {
-      for (let y = 0; y < MAP_HEIGHT; y++) {
-          const noiseValue = (noise.noise(x * NOISE_SCALE, y * NOISE_SCALE) + 1) / 2
-          
-          let terrainType = TERRAIN_TYPES.GRASS
+    for (let y = 0; y < MAP_HEIGHT; y++) {
+        const noiseValue = (noise.noise(x * NOISE_SCALE, y * NOISE_SCALE) + 1) / 2
+        
+        let terrainType = TERRAIN_TYPES.GRASS
 
-          //if (noiseValue < TERRAIN_THRESHOLD.WATER) terrainType = TERRAIN_TYPES.WATER
-          /*else*/ if (noiseValue < TERRAIN_THRESHOLD.ROCK) terrainType = TERRAIN_TYPES.ROCK
-          if (noiseValue < TERRAIN_THRESHOLD.TREE) terrainType = TERRAIN_TYPES.TREE
+        if (noiseValue < TERRAIN_THRESHOLD.WATER) terrainType = TERRAIN_TYPES.WATER
+        else if (noiseValue < TERRAIN_THRESHOLD.ROCK) terrainType = TERRAIN_TYPES.ROCK
+        else if (noiseValue < TERRAIN_THRESHOLD.TREE_NEXT_TO_WATER) terrainType = TERRAIN_TYPES.TREE
+        else if (noiseValue < TERRAIN_THRESHOLD.GRASS_NEXT_TO_WATER) terrainType = TERRAIN_TYPES.GRASS
+        else if (noiseValue < TERRAIN_THRESHOLD.SAND) terrainType = TERRAIN_TYPES.SAND
+        else if (noiseValue < TERRAIN_THRESHOLD.GRASS) terrainType = TERRAIN_TYPES.GRASS
+        else if (noiseValue < TERRAIN_THRESHOLD.TREE) terrainType = TERRAIN_TYPES.TREE
 
           // Randomize sprite within the terrain type's sprite range
-          const spriteX = Math.floor(Math.random() * 
-              (terrainType.spriteRange.x[1] - terrainType.spriteRange.x[0] + 1)) + 
-              terrainType.spriteRange.x[0]
-          const spriteY = Math.floor(Math.random() * 
-              (terrainType.spriteRange.y[1] - terrainType.spriteRange.y[0] + 1)) + 
-              terrainType.spriteRange.y[0]
+          // const spriteX = Math.floor(Math.random() * 
+          //     (terrainType.spriteRange.x[1] - terrainType.spriteRange.x[0] + 1)) + 
+          //     terrainType.spriteRange.x[0]
+          // const spriteY = Math.floor(Math.random() * 
+          //     (terrainType.spriteRange.y[1] - terrainType.spriteRange.y[0] + 1)) + 
+          //     terrainType.spriteRange.y[0]
 
           map[x][y] = {
+              type: terrainType.type,
               weight: terrainType.weight,
-              sprite: offscreenSprite(sprites[spriteX][spriteY], SPRITE_SIZE),
-              back: (terrainType !== TERRAIN_TYPES.GRASS) 
-                  ? offscreenSprite(sprites[Math.floor(Math.random() * 3)][Math.floor(Math.random() * 3)], SPRITE_SIZE) 
-                  : null
+              // sprite: offscreenSprite(sprites[spriteX][spriteY], SPRITE_SIZE),
+              // back: (terrainType !== TERRAIN_TYPES.GRASS) 
+              //     ? offscreenSprite(sprites[Math.floor(Math.random() * 3)][Math.floor(Math.random() * 3)], SPRITE_SIZE) 
+              //     : null
           }
       }
+  }
+}
+
+const assignSpritesOnMap = async () => {
+  // Terrain type parameters
+  const TERRAIN_TYPES = {
+    WATER: { type: 'WATER', spriteRange: { x: [0, 0], y: [17, 17] } },
+    ROCK: { type: 'ROCK', spriteRange: { x: [0, 1], y: [26, 26] } },
+    TREE: { type: 'TREE', spriteRange: { x: [2, 3], y: [26, 27] } },
+    GRASS: { type: 'GRASS', spriteRange: { x: [0, 2], y: [0, 2] } },
+    SAND: { type: 'SAND', spriteRange: { x: [3, 3], y: [3, 3] } },
+  }
+
+  for (let x = 0; x < MAP_WIDTH; x++) {
+    for (let y = 0; y < MAP_HEIGHT; y++) {
+      const terrainType = TERRAIN_TYPES[map[x][y].type]
+      const grassSprite =
+        sprites[
+          Math.floor(Math.random() * (TERRAIN_TYPES.GRASS.spriteRange.x[1] - TERRAIN_TYPES.GRASS.spriteRange.x[0] + 1)) +
+            TERRAIN_TYPES.GRASS.spriteRange.x[0]
+        ][
+          Math.floor(Math.random() * (TERRAIN_TYPES.GRASS.spriteRange.y[1] - TERRAIN_TYPES.GRASS.spriteRange.y[0] + 1)) +
+            TERRAIN_TYPES.GRASS.spriteRange.y[0]
+        ]
+      let spriteX, spriteY
+      switch (map[x][y].type) {
+        case TERRAIN_TYPES.GRASS.type:
+          spriteX = Math.floor(Math.random() * 
+              (terrainType.spriteRange.x[1] - terrainType.spriteRange.x[0] + 1)) + 
+              terrainType.spriteRange.x[0]
+          spriteY = Math.floor(Math.random() * 
+              (terrainType.spriteRange.y[1] - terrainType.spriteRange.y[0] + 1)) + 
+              terrainType.spriteRange.y[0]
+          map[x][y].sprite = offscreenSprite(sprites[spriteX][spriteY], SPRITE_SIZE)
+          break;
+        case TERRAIN_TYPES.TREE.type:
+          spriteX = Math.floor(Math.random() * 
+              (terrainType.spriteRange.x[1] - terrainType.spriteRange.x[0] + 1)) + 
+              terrainType.spriteRange.x[0]
+          spriteY = Math.floor(Math.random() * 
+              (terrainType.spriteRange.y[1] - terrainType.spriteRange.y[0] + 1)) + 
+              terrainType.spriteRange.y[0]
+          map[x][y].sprite = offscreenSprite(sprites[spriteX][spriteY], SPRITE_SIZE)
+          map[x][y].back = offscreenSprite(grassSprite, SPRITE_SIZE)
+          break;
+        case TERRAIN_TYPES.ROCK.type:
+          spriteX = Math.floor(Math.random() * 
+              (terrainType.spriteRange.x[1] - terrainType.spriteRange.x[0] + 1)) + 
+              terrainType.spriteRange.x[0]
+          spriteY = Math.floor(Math.random() * 
+              (terrainType.spriteRange.y[1] - terrainType.spriteRange.y[0] + 1)) + 
+              terrainType.spriteRange.y[0]
+          map[x][y].sprite = offscreenSprite(sprites[spriteX][spriteY], SPRITE_SIZE)
+          map[x][y].back = offscreenSprite(grassSprite, SPRITE_SIZE)
+          break;
+        case TERRAIN_TYPES.SAND.type:
+          spriteX = Math.floor(Math.random() * 
+              (terrainType.spriteRange.x[1] - terrainType.spriteRange.x[0] + 1)) + 
+              terrainType.spriteRange.x[0]
+          spriteY = Math.floor(Math.random() * 
+              (terrainType.spriteRange.y[1] - terrainType.spriteRange.y[0] + 1)) + 
+              terrainType.spriteRange.y[0]
+          map[x][y].sprite = offscreenSprite(sprites[spriteX][spriteY], SPRITE_SIZE)
+          break;
+        case TERRAIN_TYPES.WATER.type:
+          spriteX = Math.floor(Math.random() * 
+              (terrainType.spriteRange.x[1] - terrainType.spriteRange.x[0] + 1)) + 
+              terrainType.spriteRange.x[0]
+          spriteY = Math.floor(Math.random() * 
+              (terrainType.spriteRange.y[1] - terrainType.spriteRange.y[0] + 1)) + 
+              terrainType.spriteRange.y[0]
+          map[x][y].sprite = offscreenSprite(sprites[spriteX][spriteY], SPRITE_SIZE)
+          break;
+        default:
+          break;
+      }
+    }
   }
 }
 
@@ -351,6 +435,7 @@ onload = async (e) => {
     await generateMap()
   } while(isMapCorrect() === false)
   
+  assignSpritesOnMap()
 
   // Smoothly remove the splashscreen and launch the game
   setTimeout(() => {
