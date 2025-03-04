26856f7d0796ab359e5d86112788b0c90921b126
Dorian Bayart <dorian.bayart@gmail.com>
2025-03-04 09:12:46 +0100

    Try to better handle mobile screen viewport

--- games/pixel-fortress/js/maps.js
+++ games/pixel-fortress/js/maps.js
@@ -8,8 +8,8 @@ import { SPRITE_SIZE } from 'sprites'
 //const MAP_WIDTH = (globalThis.innerWidth > globalThis.innerHeight ? globalThis.innerWidth / SPRITE_SIZE * 1.25 : globalThis.innerHeight /1.5 / SPRITE_SIZE) / 2 | 0
 // const MAP_WIDTH = window.innerWidth > window.innerHeight ? 48 : 24
 // const MAP_HEIGHT = MAP_WIDTH * window.innerHeight / window.innerWidth | 0
-const MAP_WIDTH = window.innerWidth * (window.devicePixelRatio || 1) / SPRITE_SIZE | 0
-const MAP_HEIGHT = window.innerHeight * (window.devicePixelRatio || 1) / SPRITE_SIZE | 0
+const MAP_WIDTH = (window.visualViewport?.width ?? window.innerWidth) * (window.devicePixelRatio || 1) / SPRITE_SIZE | 0
+const MAP_HEIGHT = (window.visualViewport?.height ?? window.innerHeight) * (window.devicePixelRatio || 1) / SPRITE_SIZE | 0
 const MAX_WEIGHT = 99999999
 
 console.log(`Map: ${MAP_WIDTH} x ${MAP_HEIGHT}`)
