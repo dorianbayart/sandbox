aeda3c7da8a02493940102293cdfd7f9b3a1ff3f
Dorian Bayart <dorian.bayart@gmail.com>
2025-09-24 16:43:52 +0200

    [LinkInBio] Optimize for performance

--- link-in-bio/js/script.js
+++ link-in-bio/js/script.js
@@ -3,6 +3,11 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Defer the loading of the profile picture video
+  const profilePic = document.getElementById('profilePictureWebm')
+  profilePic.setAttribute('src', profilePic.getAttribute('data-src'))
+  setTimeout(() => document.getElementById('profilePicture').load(), 750)
+
   // Update the footer with the current year date
   const currentYear = new Date().getFullYear()
   document.getElementById('footer').innerHTML = `&copy; ${currentYear} - Made with &#9829; by <a href="https://github.com/dorianbayart" rel="me" title="DorianBayart on GitHub" target="_blank">0xDBA</a>`
